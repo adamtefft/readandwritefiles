9d76c15b00199fe532939ac11c14a6aa7f58760e
adamtefft <adam_tefft1@baylor.edu>
2023-01-30 23:04:45 -0600

    updated the file

--- customer_sales_report.py
+++ customer_sales_report.py
@@ -7,11 +7,11 @@ outfile = open("salesreport.csv", "w")
 customer_ID = '250'
 cust_total = 0
 
-outfile.write("ID  |  Total\n")
+outfile.write("ID   |  Total\n")
 
 for i in infile:
     if customer_ID != i[0]:
-        outfile.write(customer_ID + "\t" + str(cust_total) + "\n")
+        outfile.write(customer_ID + "\t\t" + str("%.2f" % cust_total) + "\n")
         # outfile.write(str(cust_total))
 
         customer_ID = i[0]
@@ -20,6 +20,6 @@ for i in infile:
     total = float(i[3]) + float(i[4]) + float(i[5])
     cust_total += total
 
-outfile.write(customer_ID + "\t" + str(cust_total) + "\n")
+outfile.write(customer_ID + "\t\t" + str("%.2f" % cust_total) + "\n")
 
 outfile.close()
